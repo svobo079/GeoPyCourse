6c9111c24525a8156767d3e4bc78f525043eebb0
svobo079 <svobo079@alumni.umn.edu>
2025-03-25 14:08:15 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -33,7 +33,7 @@ nitpicky = True
 show_warning_types = True
 
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_theme_path = ['_themes']
 html_css_files = [
     # 'basic.css',  # included through inheritance from the basic theme
